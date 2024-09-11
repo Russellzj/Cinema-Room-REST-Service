b77ed5e40676592aa38c19b99afa20336f336384
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-11 19:55:44 -0400

    Initial commit

---
+++ src/cinema/TaskController.java
@@ -0,0 +1,17 @@
+package cinema;
+
+import cinema.room.Theatre;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+
+    @GetMapping(path = "/seats", produces = "application/json")
+    public cinema.room.Seat[] getSeats() {
+        Theatre myTheatre = new Theatre();
+        return myTheatre.getSeats();
+    }
+
+
+}
