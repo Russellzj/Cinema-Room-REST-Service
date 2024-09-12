17b2cfc99ffb7b1ea9c2afe6927f84d9dc64d837
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-12 00:59:32 -0400

    Completed instatiation and filling of seat array

--- src/cinema/room/Theatre.java
+++ src/cinema/room/Theatre.java
@@ -3,12 +3,15 @@ package cinema.room;
 public class Theatre {
     private final int ROWS = 9;
     private final int COLUMNS = 9;
-    private Seat[] seats = new Seat[ROWS * COLUMNS];
+    private Seat[] seats = new Seat[(ROWS * COLUMNS)];
 
     {
-        for(int i = 1; i < ROWS; i++) {
-            for(int j = 1; i < COLUMNS; j++){
-                seats[i - 1 + j - 1] = new Seat(i, j);
+        for(int i = 1; i <= ROWS; i++) {
+            for(int j = 1; j <= COLUMNS; j++){
+                //System.out.print((i - 1) * (ROWS) + (j - 1) + ": ");
+                //System.out.println(i + " " + j);
+                seats[(i - 1) * (ROWS) + (j - 1)] = new Seat(i, j);
+
             }
         }
     }
